1082d1f6a5a93cc7347ac7646998a25aacdf5830
daniella97 <daniella.friedman@mail.huji.ac.il>
2020-11-01 22:49:39 +0200

    update

---
+++ PycharmProjects/nandAssembler/Assembler.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+
+def parse_file(filename):
+    with open(filename, 'r') as infile:
+        asm_lines = []
+        for line in infile:
+            # skip empty lines
+            if len(line.split()) == 0:
+                continue
+            else:
+                asm_lines.append(line)
+
+
+def check_input(file_path):
+    # checks if the path given exists
+    if os.path.exists(file_path):
+        # checks if the path leads to a file
+        if os.path.isfile(file_path):
+            parse_file(file_path)
+        # checks if the path leads to a directory
+        elif os.path.isdir(file_path):
+            files = os.listdir(file_path)
+            # parse each file in the directory
+            for file in files:
+                parse_file(file)
+
+
+def main():
+    file_path = sys.argv[1]
+    check_input(file_path)
+
+
+if __name__ == '__main__':
+    main()
---
+++ PycharmProjects/nandAssembler/SymbolTable.py
@@ -0,0 +1,21 @@
+class SymbolTable:
+    def __init__(self):
+        self.symbol_table = {}
+        self.__initialize_table()  # initializes table with pre-defined symbols
+
+    def __initialize_table(self):
+        # initialize pre-defined symbols
+        self.symbol_table["SCREEN"] = 16384
+        self.symbol_table["KBD"] = 24576
+        self.symbol_table["SP"] = 0
+        self.symbol_table["LCL"] = 1
+        self.symbol_table["ARG"] = 2
+        self.symbol_table["THIS"] = 3
+        self.symbol_table["THAT"] = 4
+
+        # add 16 built in registers
+        for i in range(0, 16):
+            self.symbol_table["R" + str(i)] = i
+
+    def get_symbol_table(self):
+        return self.symbol_table
