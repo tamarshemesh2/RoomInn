bee0e04d5c03dc7a54c8bcf3a3accf86bec1adb1
daniella97 <daniella.friedman@mail.huji.ac.il>
2020-11-03 23:09:18 +0200

    statics

--- PycharmProjects/nandAssembler/Parser.py
+++ PycharmProjects/nandAssembler/Parser.py
@@ -5,6 +5,15 @@ from enum import Enum
 S_PATTERN = re.compile('[AMD]([<>]{2})')
 INIT_COUNTER = 0
 A_COMMAND_START = '@'
+INVALID_COMMAND = "Invalid command"
+COMMAND_IDENTIFIER = 0
+EQUAL_SIGN = "="
+EMPTY = ""
+JUMP_PREFIX = ";"
+COMP_INDEX_IN_JMP = 0
+JMP_INDEX = -1
+COMP_INDEX_IN_EQUAL = -1
+DEST_INDEX_IN_EQUAL = 0
 # ####################################################################################################
 
 
@@ -27,11 +36,11 @@ class Parser:
         if self.has_more_commands():
             self._currentCommand += 1
             return
-        return Exception("Invalid command")
+        return Exception(INVALID_COMMAND)
 
     def command_type(self):
         cur = self._commands[self._currentCommand]
-        if cur[0] == A_COMMAND_START:
+        if cur[COMMAND_IDENTIFIER] == A_COMMAND_START:
             return CommandTypes.A
         if re.search(S_PATTERN, cur) is not None:
             return CommandTypes.Shift
@@ -40,27 +49,28 @@ class Parser:
     def symbol(self):
         cur = self._commands[self._currentCommand]
         if self.command_type() == CommandTypes.A:
+            # without @
             return cur[1:]
-        return Exception("Invalid command")
+        return Exception(INVALID_COMMAND)
 
     def dest(self):
         cur = self._commands[self._currentCommand]
         if self.command_type() != CommandTypes.A:
-            if re.compile("=").search(cur) is not None:
-                return cur.split('=')[0]
-            return ""
+            if re.compile(EQUAL_SIGN).search(cur) is not None:
+                return cur.split(EQUAL_SIGN)[DEST_INDEX_IN_EQUAL]
+            return EMPTY
 
     def comp(self):
         cur = self._commands[self._currentCommand]
         if self.command_type() == CommandTypes.C:
-            return (cur.split('=')[-1]).split(';')[0]
+            return (cur.split(EQUAL_SIGN)[COMP_INDEX_IN_EQUAL]).split(JUMP_PREFIX)[COMP_INDEX_IN_JMP]
 
     def jump(self):
         cur = self._commands[self._currentCommand]
         if self.command_type() != CommandTypes.A:
-            if re.compile(";").search(cur) is not None and cur[-1] != ';':
-                return cur.split(';')[-1]
-            return ""
+            if re.compile(JUMP_PREFIX).search(cur) is not None and cur[JMP_INDEX] != JUMP_PREFIX:
+                return cur.split(JUMP_PREFIX)[JMP_INDEX]
+            return EMPTY
 
     def shift(self):
         cur = self._commands[self._currentCommand]
