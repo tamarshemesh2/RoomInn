33ce83ad342f9cf4ad022ec0e0689bb98afd91d5
daniella97 <daniella.friedman@mail.huji.ac.il>
2020-11-04 21:40:59 +0200

    ready

--- PycharmProjects/nandAssembler/SymbolTable.py
+++ PycharmProjects/nandAssembler/SymbolTable.py
@@ -15,13 +15,19 @@ NON_BUILT_IN_START = 16
 
 
 class SymbolTable:
+    """
+    SymbolTable class- contains the table with all the symbols and their addresses in an instruction file,
+    this includes built in symbols and user defined symbols i.e. labels and variables
+    """
     def __init__(self):
         self._counter = NON_BUILT_IN_START
         self._symbol_table = {}
-        self.__initialize_table()  # initializes table with pre-defined symbols
+        self._initialize_table()  # initializes table with pre-defined symbols
 
-    def __initialize_table(self):
-        # initialize pre-defined symbols
+    def _initialize_table(self):
+        """
+        initializes the symbol table with built in symbols
+        """
         self.add_entry(SCREEN_SYMBOL[NAME], SCREEN_SYMBOL[ADDRESS])
         self.add_entry(KBD_SYMBOL[NAME], KBD_SYMBOL[ADDRESS])
         self.add_entry(SP_SYMBOL[NAME], SP_SYMBOL[ADDRESS])
@@ -35,17 +41,38 @@ class SymbolTable:
             self.add_entry(BUILT_IN_REGISTER + str(i), i)
 
     def add_entry(self, symbol, address):
+        """
+        adds the given symbol and address to the symbol table if the table doesn't already contain it
+        @param symbol: the symbol to add to the table
+        @param address: the address for the given symbol
+        """
         if not self.contains(symbol):
             self._symbol_table[symbol] = address
 
     def advance_symbol_counter(self):
+        """
+        advances the symbol counter by 1, this is only performed of a variable is added to the table
+        """
         self._counter += 1
 
     def contains(self, symbol):
+        """
+        checks if a given symbol is contained in the symbol table
+        @param symbol: the symbol to look for
+        @return: true if the table contains the symbol, false otherwise
+        """
         return symbol in self._symbol_table
 
     def get_address(self, symbol):
+        """
+        returns the address of a given symbol
+        @param symbol: the symbol to get the address of
+        @return: the address
+        """
         return self._symbol_table[symbol]
 
     def get_counter(self):
+        """
+        @return: returns the symbol counter
+        """
         return self._counter
